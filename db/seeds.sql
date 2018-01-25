3d5ff8135b32d3ed33f515e4fe34dc9cf24b683f
John LaTona <johnlatona@Johns-MBP.fios-router.home>
2018-01-24 19:13:01 -0500

    added seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheeseburger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Cheeseburger", 0);
